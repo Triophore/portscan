efd40e7d8f139f9c8fc94217ede0008838a3ffc1
ls <venu@lifesignals.com>
2023-12-20 16:51:47 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -51,14 +51,14 @@ async fn main() {
 
 
 async fn full_scan(IPAddress : String){
-    let target = IPAddress.as_str(); // Change this to the IP address you want to scan
-    let timeout = Duration::from_millis(300); // Adjust the timeout duration as needed
+    let target = IPAddress.clone(); // Change this to the IP address you want to scan
+    let timeout = Duration::from_millis(100); // Adjust the timeout duration as needed
 
     println!("Starting to download ports DB !");
 
     let case_study_url = format!("https://raw.githubusercontent.com/Triophore/portscan/main/ports.json");
 
-    println!("Encoded URL {}",case_study_url.clone());
+    // println!("Encoded URL {}",case_study_url.clone());
 
     match reqwest::get(case_study_url).await {
          Ok(resp) => {
@@ -70,29 +70,54 @@ async fn full_scan(IPAddress : String){
 
             let mut Res : HashMap<String,String> = HashMap::new();
         
-            let port_range = 1..=65535;
+            let port_range = 1..=255;
 
-            for port in port_range {
-                let target_address = format!("{}:{}", target, port);
-                match target_address.to_socket_addrs() {
-                    Ok(mut addrs) => {
-                        if let Some(target_addr) = addrs.next() {
-                            //println!("Trying on {}",target_addr.clone());
-                            if let Ok(socket) = TcpStream::connect_timeout(&target_addr,timeout) {
-                                println!("Port {} is open", port);
-                                // Additional actions for an open port can be added here
-                                let inf =json.get(port.to_string());
-                                if inf.is_some() {
-                                    println!("{:?}",inf.unwrap().as_array()[0]);
+            let start = 1;
+            let end = 65536;
+            let group_size = 256;
+            
+
+            println!("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            for chunk_start in (start..=end).step_by(group_size) {
+                let chunk_end = (chunk_start + group_size - 1).min(end);
+                // println!("Chunk: {} to {}", chunk_start, chunk_end);
+                let new_json = json.clone();
+                let _target = target.clone();
+                tokio::spawn(async move {
+                    for eve in (chunk_start..=chunk_end) {
+                        let target_address = format!("{}:{}",_target.as_str(), eve);
+                        let new_target = target_address.clone();
+
+                        match new_target.to_socket_addrs() {
+                            Ok(mut addrs) => {
+                                if let Some(target_addr) = addrs.next() {
+                                    //println!("Trying on {}",target_addr.clone());
+                                    if let Ok(socket) = TcpStream::connect_timeout(&target_addr,timeout) {
+                                        // println!("Port {} is open", eve);
+                                        // Additional actions for an open port can be added here
+                                        let inf =new_json.get(eve.to_string());
+                                        if inf.is_some() {
+                                            let arr = inf.unwrap().as_array().unwrap();
+                                            println!("----------------------------------------------------------------------------------------------------");
+                                            println!("Port         {}    is open               :: {:?}",eve,arr[0]["description"].as_str().unwrap());
+                                            println!("----------------------------------------------------------------------------------------------------");
+                                        }
+                                    }else{
+                                        // println!("Port {} is closed", eve);
+                                    }
                                 }
                             }
+                            Err(e) => {
+                                println!("Failed to resolve the target address for port {}: {}", eve, e);
+                            }
                         }
+                      
                     }
-                    Err(e) => {
-                        println!("Failed to resolve the target address for port {}: {}", port, e);
-                    }
-                }
+                    
+                }); 
             }
+            // println!("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
          }
          Err(err) => {
             println!("Reqwest Error: {}", err)
